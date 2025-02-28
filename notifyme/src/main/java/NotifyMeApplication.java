1a97ab5de258885f376e36927261bcbab10f7ce4
akashkore <akash.kore@antrepriz.com>
2025-02-28 12:20:11 +0530

    classes

--- notifyme/src/main/java/NotifyMeApplication.java
+++
@@ -1,9 +0,0 @@
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class NotifyMeApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(NotifyMeApplication.class, args);
-    }
-}
